69493d1b67538f8b1bf8e3a264f0e80f0fdadd01
tangliwei <3250900470@qq.com>
2024-11-17 16:05:39 +0800

    前端页面完善

--- src/app/components/dialog/dialog-message-actions.tsx
+++ src/app/components/dialog/dialog-message-actions.tsx
@@ -7,8 +7,8 @@ import {GptVersion} from '../../constants'
 import {SessionConfig} from "@/types/chat";
 import { CSSProperties, useRef, useState } from 'react';
 
-//
-//
+
+
 export function Action(props: {
     icon: JSX.Element;
     onClick?: () => void;
